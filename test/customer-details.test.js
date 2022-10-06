7b4ed9d165d6a88f013538c3163508aaf00e888b
johnghosh <46923786+johnghosh@users.noreply.github.com>
2022-10-06 11:49:28 +0530

    Update customer-details.test.js

--- test/customer-details.test.js
+++ test/customer-details.test.js
@@ -4,6 +4,7 @@ import { stub ,spy} from 'sinon';
 import '../src/Customer/Customer-details.js';
 
 
+
 describe('customer details', () => {
   // Write test cases inside this block
   let element;
@@ -22,37 +23,53 @@ describe('customer details', () => {
   it("Customer details Renders",()=>{
     expect(element).to.be.an('HTMLElement');
   });
-  it("submitHandler function test",()=>{
-    let save = stub(element,"submitHandler");
-    let result = '';
-    let data = {}
-    save.withArgs(data).returns(result);
-    expect(element.submitHandler(data)).to.be.equal('')
+  it("submitHandler function test",async()=>{
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth();
+    const day = today.getDate();
+    
+    const mindate = new Date(year - 80, month, day);
+    const maxdate = new Date(year - 18, month, day);
+    const fstVal = element.shadowRoot.querySelector('#first_name');
+    const lastNameVal = element.shadowRoot.querySelector('#last_name');
+   const dateOfBirth = element.shadowRoot.querySelector('#dateof_birth');
+    const email = element.shadowRoot.querySelector('#email');
+    const mobileNo = element.shadowRoot.querySelector('#mobile_number');
+    const salary = element.shadowRoot.querySelector('#monthly_salary');
+    const emi = element.shadowRoot.querySelector('#EMIs_amount');
+    const check = element.shadowRoot.querySelector('#terms');
+    fstVal.modelValue = "john";
+    lastNameVal.modelValue = "ghosh";
+    dateOfBirth.modelValue = new Date(maxdate);
+    email.modelValue = "john@gmail.com";
+    mobileNo.modelValue ="7797553117";
+    salary.modelValue =450;
+    emi.modelValue =40;
+    check.modelValue = true;
+     //  let obj ={status:200}
+    // let save = stub(element,"submitHandler");
+    // const buttonClick = element.shadowRoot.querySelector('#nextbtn');
+    // buttonClick.click();
+    // save.onFirstCall().returns(obj)
+   
+    // setTimeout(()=>{
+    //   expect(save).to.be.equal(obj)
+    // },1000)
+    let myFunctionStub = stub(window,"fetch");
+    //myFunctionStub.onFirstCall().returns(obj)
+    element.requestUpdate();
+    await element.updateComplete;
+    element.shadowRoot.querySelector('#nextbtn').click();
+    expect(myFunctionStub).to.have.callCount(1);
+    //  expect(myFunctionStub).to.have.callCount(1);
   })
-  it("_toEmidetails function check",()=>{
+  it("_toEmidetails function check",async()=>{
     const save = spy(element,"_toEmidetails");
+    element.requestUpdate();
+    await element.updateComplete;
     let button = element.shadowRoot.querySelector('.backbg-btn-color');
     button.click();
-    element.updateComplete.then(() => {
     expect(save).to.be.calledOnce;
   });
-    // save.restore();
-    // assert.calledWith(save,result)
-  });
-  // it("Mock and stub api call", (done) =>{
-  //   var obj = { id: 123 };
-  //   const scope = nock("https://loanfeapi.herokuapp.com")
-  //     .post("/submit-form")
-  //     .reply(200, obj);
-  //     element
-  //     .submitHandler()
-  //     .then((result)=> {
-  //       expect(result).to.be.eql(obj);
-  //       done();
-  //     })
-  //     .catch(error => {
-  //       done(new Error("test case failed: " + error));
-  //     });
-  // });
 });
-
